a35498b4bb75ca4c20244684fb8d0de9459f043e
leandro machado <fahurleandro@gmail.com>
2021-01-09 13:24:53 -0800

    first commit

---
+++ code/FirstDialog/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        JOptionPane.showMessageDialog(null,"Welcome to Java");
+    }
+}
